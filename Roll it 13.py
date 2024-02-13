9a083264b9b7e742ebbcf88e4edeafbef089dab1
CaseyBradyMHS <140982529+CaseyBradyMHS@users.noreply.github.com>
2024-02-14 11:05:52 +1300

    Add files via upload

--- Roll it 13.py
+++ Roll it 13.py
@@ -13,28 +13,18 @@ def statement_generator(text, decoration):
     return ""
 
 
+def yes_no(question):
+    # ask the user if they want to read the instructions.
+    answer = input(question).lower()
+    if answer == "yes" or "y":
+        print("yes was chosen")
+    if answer == "no" or "n":
+        print("no was chosen")
+
+
 # main routine
-yes_dictionary = {
-    "yes",
-    "YES",
-    "Yes"
-}
-no_dictionary = {
-    "no",
-    "NO",
-    "No"
-}
 error = "please choose a valid answer"
 keep_going = ""
 while keep_going == "":
-
-    # ask the user if they want to read the instructions.
-    want_instructions = input("Would you like to read the instructions? ")
-    if want_instructions in yes_dictionary:
-        print("answer yes was chosen")
-        keep_going = input("press enter to go again or any key to stop. ")
-    elif want_instructions in no_dictionary:
-        print("answer no was chosen")
-        keep_going = input("press enter to go again or any key to stop. ")
-    else:
-        print(error)
+    yes_no("would you like to read the instructions? ")
+    
